2c07ec0c4e3f1ef8cc23edc6d1b5d83872ce7c3c
Eduardo Sa <nofrereis@gmail.com>
2020-01-16 20:41:28 +0000

    Update H2Service.java

--- java-h2-jpa-exemplo/src/main/java/com/h2/jpa/exemplo/service/H2Service.java
+++ java-h2-jpa-exemplo/src/main/java/com/h2/jpa/exemplo/service/H2Service.java
@@ -1,5 +1,13 @@
 package com.h2.jpa.exemplo.service;
 
+
+@Service
 public class H2Service {
 
+  @Autowired
+	private H2Repository h2Repository;
+  
+  public List<H2Model> recuperarTodosTudo() {
+		return h2Repository.findAll();
+	}
 }
