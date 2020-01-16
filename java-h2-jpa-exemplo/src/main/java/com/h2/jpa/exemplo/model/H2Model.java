54b66817f17a061af40f0bbe1c75c5d133fab98a
Eduardo Sa <nofrereis@gmail.com>
2020-01-16 20:24:41 +0000

    Update H2Model.java

--- java-h2-jpa-exemplo/src/main/java/com/h2/jpa/exemplo/model/H2Model.java
+++ java-h2-jpa-exemplo/src/main/java/com/h2/jpa/exemplo/model/H2Model.java
@@ -1,5 +1,23 @@
 package com.h2.jpa.exemplo.model;
 
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Table(name = "tabelah2")
 public class H2Model {
 
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", length = 9)
+	private Long id;
+
+	@Column(name = "nome", length = 50)
+	private String nome;
+
+	@Column(name = "idade", length = 2)
+	private Integer idade;
 }
