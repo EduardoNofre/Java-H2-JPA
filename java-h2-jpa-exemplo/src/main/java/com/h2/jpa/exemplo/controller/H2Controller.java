6dae670274a348fe389c50d56ef3ba2bd28df144
Eduardo Sa <nofrereis@gmail.com>
2020-01-16 20:41:43 +0000

    Update H2Controller.java

--- java-h2-jpa-exemplo/src/main/java/com/h2/jpa/exemplo/controller/H2Controller.java
+++ java-h2-jpa-exemplo/src/main/java/com/h2/jpa/exemplo/controller/H2Controller.java
@@ -1,5 +1,21 @@
 package com.h2.jpa.exemplo.controller;
 
+
+@Api(value = "/v1/h2", description = "Responsável por Aluno")
+@RestController
+@Validated
+@RequestMapping("/controlerh2")
+@CrossOrigin(origins="*")
 public class H2Controller {
 
+  @Autowired
+	private H2Service h2Service;
+  
+  @GetMapping(path = "/consultar", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity buscaTudo() {
+
+		List<H2Model> h2ModelResponse = h2Service.recuperarTodosTudo();
+
+		return new ResponseEntity<>(h2ModelResponse, HttpStatus.OK);
+	}
 }
